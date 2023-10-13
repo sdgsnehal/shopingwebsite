502148ef7595125d44cf635a0da7a1309b1ae2e9
snehal gaurkhede <sdgsnehal@gmail.com>
2023-10-13 16:37:16 +0530

    footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
-
+import payment from "../../../img/payment.png";
+import "./Footer.scss"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati ducimus, odit perferendis ipsa, exercitationem voluptatem similique tenetur iste blanditiis adipisci a quis modi eum ab asperiores repellendus facilis quasi error?</span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed non magni distinctio dolorum cupiditate adipisci mollitia id hic corporis, dolor aperiam animi esse eos suscipit ab similique accusamus deserunt cum.</span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Scriptedstyles</span>
+          <span className="copyright">Copyright 2023. All Rights Reserved</span>
+          
+        </div>
+        <div className="right">
+          <img src={payment} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
